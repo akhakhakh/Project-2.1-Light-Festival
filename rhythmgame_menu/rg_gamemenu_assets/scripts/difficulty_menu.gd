bf66ebe6c2f5272b2f6a017f57e039247db06b1d
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-10-31 03:16:38 +0100

    added connection to both pages

--- rhythmgame_menu/rg_gamemenu_assets/scripts/difficulty_menu.gd
+++ rhythmgame_menu/rg_gamemenu_assets/scripts/difficulty_menu.gd
@@ -13,12 +13,18 @@ signal back_pressed
 
 #animation-ref
 @onready var animation_player = $AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	easy_button.pressed.connect(_on_easy_button_pressed())
-	medium_button.pressed.connect(_on_medium_button_pressed())
-	hard_button.pressed.connect(_on_hard_button_pressed())
-	back_button.pressed.connect(_on_back_button_pressed())
+	easy_button.pressed.connect(_on_easy_button_pressed)
+	medium_button.pressed.connect(_on_medium_button_pressed)
+	hard_button.pressed.connect(_on_hard_button_pressed)
+	
+	#connection debug
+	if back_button:
+		back_button.pressed.connect(_on_back_button_pressed)
+	else:
+		print("ERROR: BackButton node not found!")
 
 func play_enter_animation():
 	animation_player.play("bounce_in_from_right") #called by main_page.gd for transition
@@ -39,5 +45,6 @@ func _on_hard_button_pressed():
 	emit_signal("hard_selected")
 
 func _on_back_button_pressed():
-	print("Going back to menu")
+	print("Returning to Menu")
 	emit_signal("back_pressed")
+	get_tree().change_scene_to_file("res://rhythmgame_menu/rg_gamemenu_assets/scenes/main_page.tscn")
--- rhythmgame_menu/rg_gamemenu_assets/scripts/main_page.gd
+++ rhythmgame_menu/rg_gamemenu_assets/scripts/main_page.gd
@@ -1,17 +1,16 @@
 extends Node2D
 
 #Reference
-var startup_page: Control
+@onready var startup_page: Control
+@onready var difficulty_menu = preload("res://rhythmgame_menu/rg_gamemenu_assets/scenes/difficulty_menu.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#reference startup page
 	startup_page = $CanvasLayer/StartUpPage
-	
 	startup_page.ready.connect(_on_startup_page_ready)
 	
 	print("Main scene loaded - Pixel Rhythm Game")
-	
 
 func _on_startup_page_ready():
 	# Connect signals from startup page
@@ -23,7 +22,7 @@ func _on_startup_page_ready():
 
 
 func _on_start_button_pressed():
-	print("Transitioning to difficulty selection...")
+	print("Transitioning to difficulty menu...")
 	get_tree().change_scene_to_file("res://rhythmgame_menu/rg_gamemenu_assets/scenes/difficulty_menu.tscn")
 
 func _on_leaderboard_button_pressed():
