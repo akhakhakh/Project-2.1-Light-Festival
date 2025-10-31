f9fd5de308f4ba6387c9bc2bb4dff3b2a0f71bcf
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-10-31 02:56:32 +0100

    added difficulty menu for the game

---
+++ rhythmgame_menu/rg_gamemenu_assets/scripts/difficulty_menu.gd
@@ -0,0 +1,43 @@
+extends Control
+
+signal easy_selected
+signal medium_selected
+signal hard_selected
+signal back_pressed
+
+#reference
+@onready var easy_button = $EasyButton
+@onready var medium_button = $MediumButton
+@onready var hard_button = $HardButton
+@onready var back_button = $BackButton
+
+#animation-ref
+@onready var animation_player = $AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	easy_button.pressed.connect(_on_easy_button_pressed())
+	medium_button.pressed.connect(_on_medium_button_pressed())
+	hard_button.pressed.connect(_on_hard_button_pressed())
+	back_button.pressed.connect(_on_back_button_pressed())
+
+func play_enter_animation():
+	animation_player.play("bounce_in_from_right") #called by main_page.gd for transition
+
+func play_exit_animation():
+	animation_player.play("bounce_out_to_right")
+
+func _on_easy_button_pressed():
+	print("Easy difficulty selected")
+	emit_signal("easy_selected")
+
+func _on_medium_button_pressed():
+	print("Medium difficulty selected")
+	emit_signal("medium_selected")
+
+func _on_hard_button_pressed():
+	print("Hard difficulty selected")
+	emit_signal("hard_selected")
+
+func _on_back_button_pressed():
+	print("Going back to menu")
+	emit_signal("back_pressed")
