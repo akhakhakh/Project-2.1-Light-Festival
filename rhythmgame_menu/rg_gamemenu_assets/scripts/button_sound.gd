372e9812dfd0a649eca7d9fb0f445719f959a3b2
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-10-31 15:46:10 +0100

    sorted files, made changes to links within code.

--- rhythmgame_menu/rg_gamemenu_assets/scripts/button_sound.gd
+++
@@ -1,9 +0,0 @@
-extends AudioStreamPlayer2D
-
-@onready var audio_player = $AudioStreamPlayer
-@onready var button_sound = preload("res://rhythmgame_menu/rg_gamemenu_assets/soundEffects/button_click.mp3")
-
-func play_button_sound():
-	if audio_player and button_sound:
-		audio_player.stream = button_sound
-		audio_player.play()
