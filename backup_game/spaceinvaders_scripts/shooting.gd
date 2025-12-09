4b20f0203936db1a1ec8fe35fa94416d6b183ce3
Eliza Horvath <eliza.horvath@student.nhlstenden.com>
2025-12-09 22:57:43 +0100

    added colorful laser assets

--- backup_game/spaceinvaders_scripts/shooting.gd
+++ backup_game/spaceinvaders_scripts/shooting.gd
@@ -1,16 +1,12 @@
 extends Node2D
 
-@export var laser_scenes: Array[PackedScene] = []
+@export var laser_scene:PackedScene
 
 var can_shoot = true
 
 func _input(event):
 	if Input.is_action_just_pressed("shoot") && can_shoot:
 		can_shoot = false
-		
-		# Pick random laser scene
-		var laser_scene = laser_scenes.pick_random()
-		
 		var laser = laser_scene.instantiate() as Area2D
 		laser.global_position = get_parent().global_position - Vector2(0, 20)
 		get_tree().root.get_node("main").add_child(laser)
