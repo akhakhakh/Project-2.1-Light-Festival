3f047ab7d456ebbc77b11d4820655fec664058c3
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-12-12 02:02:42 +0100

    made the lasers shot more bullets + added delay

--- backup_game/spaceinvaders_scripts/shooting.gd
+++ backup_game/spaceinvaders_scripts/shooting.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
 @export var laser_scene:PackedScene
+@export var fire_rate: float = 0.2 #seconds between shots
 
 var can_shoot = true
 
 func _input(event):
 	if Input.is_action_just_pressed("shoot") && can_shoot:
+		shoot_laser()
 		can_shoot = false
+		await get_tree().create_timer(fire_rate).timeout
+		can_shoot = true
+
+func shoot_laser():
 		var laser = laser_scene.instantiate() as Area2D
 		laser.global_position = get_parent().global_position - Vector2(0, 20)
 		get_tree().root.get_node("main").add_child(laser)
-		laser.tree_exited.connect(on_laser_destroyed)
+		#laser.tree_exited.connect(on_laser_destroyed)
 
 func on_laser_destroyed():
 	can_shoot = true
