daf4a718acb8cef1d53e11f5de890902befa9bc2
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-12-12 02:32:44 +0100

    added shooting sound effect

--- backup_game/spaceinvaders_scripts/shooting.gd
+++ backup_game/spaceinvaders_scripts/shooting.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var laser_scene:PackedScene
+@onready var shoot_sound = $AudioStreamPlayer2D
 @export var fire_rate: float = 0.2 #seconds between shots
 
 var can_shoot = true
@@ -13,6 +14,7 @@ func _input(event):
 		can_shoot = true
 
 func shoot_laser():
+		shoot_sound.play() #play sound effect
 		var laser = laser_scene.instantiate() as Area2D
 		laser.global_position = get_parent().global_position - Vector2(0, 20)
 		get_tree().root.get_node("main").add_child(laser)
